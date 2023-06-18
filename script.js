f050a854720c76613543e9b7b32d8b90ffc37fb9
bos12341 <122173560+bos12341@users.noreply.github.com>
2023-06-18 23:19:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,28 @@ function startGame() {
       clearInterval(countdownInterval);
       countdownElement.classList.add('hidden');
 
-      // TODO: 게임 시작 로직 추가
+      // 0초가 지난 후 팝업 창 생성
+      setTimeout(function() {
+        var popupWindow = window.open('', '', 'width=500,height=500');
+        popupWindow.document.write('<html><head><title>팝업 창</title></head><body></body></html>');
+
+        // 버튼 스타일
+        var buttonStyle = 'width: 200px; height: 200px; font-size: 32px; color: white; background-color: blue; border: none; border-radius: 50%;';
+
+        // 동그라미 버튼 생성
+        var button = document.createElement('button');
+        button.setAttribute('style', buttonStyle);
+        button.textContent = 'DISCORD';
+        button.addEventListener('click', function() {
+          // 내가 지정한 사이트로 이동
+          window.location.href = 'https://discord.gg/9GVAAndp5u';
+        });
+
+        // 팝업 창 가운데에 버튼 배치
+        var popupBody = popupWindow.document.body;
+        popupBody.setAttribute('style', 'display: flex; justify-content: center; align-items: center; height: 100%; margin: 0;');
+        popupBody.appendChild(button);
+      }, 0);
 
       // 예시: 5가 0이 될 때까지 동작하지 않도록 설정
       gameContainer.addEventListener('click', function(event) {
